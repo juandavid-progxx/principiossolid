93bac82606be309ec6b563ad90e76d35df13d07c
juan mosquera <112137410+juandavid-progxx@users.noreply.github.com>
2024-02-18 23:09:29 -0500

    principio single responsability

---
+++ src/main/java/co/edu/uniquindio/biblioteca/usoEstudiante.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.biblioteca;
+
+public class usoEstudiante {
+    public static void main(String[] args) {
+        estudiante estud= new estudiante("juan","jimenez",20,12666,4);
+        estud.mostrarDatos();
+
+    }
+}
